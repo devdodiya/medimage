758677a83df13368c5bb050f4ffa94351f175ca0
aj5 <peter@atomjump.com>
2016-01-31 12:45:56 -0500

    full scan

--- www/js/index.js
+++ www/js/index.js
@@ -52,6 +52,7 @@ var app = {
     takePicture: function() {
       navigator.camera.getPicture( function( imageURI ) {
         alert( imageURI );
+        this.uploadPhoto(imageURI);
       },
       function( message ) {
         alert( message );
@@ -91,5 +92,40 @@ var app = {
               }
           });
        }
+    },
+    
+    uploadPhoto: function(imageURI) {
+    
+        if(this.foundServer) {
+            var options = new FileUploadOptions();
+            options.fileKey="file";
+            options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+            options.mimeType="image/jpeg";
+ 
+            var params = new Object();
+            params.value1 = "test";
+            params.value2 = "param";
+ 
+            options.params = params;
+            options.chunkedMode = false;
+ 
+            var ft = new FileTransfer();
+            ft.upload(imageURI, this.foundServer, this.win, this.fail, options);
+            
+        } else {
+            alert('No server known');
+        }
+    },
+ 
+    win:  function(r) {
+            console.log("Code = " + r.responseCode);
+            console.log("Response = " + r.response);
+            console.log("Sent = " + r.bytesSent);
+            alert(r.response);
+    },
+ 
+    fail: function(error) {
+            alert("An error has occurred: Code = " = error.code);
     }
+
 };
